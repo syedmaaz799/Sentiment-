632de46a4d1e5c74d1de28f31daf22308419bfed
syedmaaz799 <152372260+syedmaaz799@users.noreply.github.com>
2024-05-17 13:00:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import joblib
 import numpy as np
-from keras.models import load_model
-from keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
 
 # Load the models and other components with error handling
